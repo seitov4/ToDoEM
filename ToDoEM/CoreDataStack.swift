5054812e6afa7240ba4cd67f989f5014ab89e9ba
Nurseit <161697278+seitov4@users.noreply.github.com>
2025-09-16 15:43:50 +0500

    Add Core Data model: Task entity and CoreDataStack

---
+++ ToDoEM/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  ToDoEM
+//
+//  Created by Nurseit Seitov on 16.09.2025.
+//
+
+import Foundation
---
+++ ToDoEM/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoEM
+//
+//  Created by Nurseit Seitov on 16.09.2025.
+//
+
+import Foundation
